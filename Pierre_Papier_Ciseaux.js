9feba9bb9752d61bbf5b4eb7ca4a479c3327eefb
Giomps <118890459+Giomps@users.noreply.github.com>
2023-06-17 23:25:09 +0200

    Add files via upload

---
+++ Pierre_Papier_Ciseaux.js
@@ -0,0 +1,41 @@
+function rockScissorPaper() {
+
+    let simonTextContent = document.querySelector('.simonTextContent')
+    let bryanTextContent = document.querySelector('.bryanTextContent')
+
+    const textResult = document.querySelector('.textResult');
+
+    let RPS = ["🧱", "📃", "✂️"];
+
+    let Simon = RPS[Math.floor(Math.random() * RPS.length)]
+    let Bryan = RPS[Math.floor(Math.random() * RPS.length)]
+
+    simonTextContent.textContent = `Simon dit ${Simon}`;
+    bryanTextContent.textContent = `Bryan dit ${Bryan}`;
+
+    if (Simon === "🧱" && Bryan === "✂️") {
+        textResult.textContent = `Simon a gagné`
+    
+    } else if(Simon === "🧱" && Bryan === "📃") {
+        textResult.textContent = `Bryan a gagné`
+    
+    } else if(Simon === "📃" && Bryan === "🧱") {
+        textResult.textContent = `Simon a gagné`
+    
+    } else if (Simon === "📃" && Bryan === "✂️") {
+        textResult.textContent = `Bryan a gagné`
+
+    } else if(Simon === "✂️" && Bryan === "🧱") {
+        textResult.textContent = `Bryan a gagné`
+    
+    } else if(Simon === "✂️" && Bryan === "📃") {
+        textResult.textContent = `Simon a gagné`
+
+    } else {
+        textResult.textContent = ``
+    }
+
+}
+
+setInterval(rockScissorPaper, 3500)
+    
